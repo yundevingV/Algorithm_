f4eb853a3344bf350e8cc1aa540a1f5bd4a50124
ys0222 <ggdbstjd0222@gmail.com>
2022-08-30 01:55:13 +0900

    0830

--- 2110_공유기설치.py
+++ 2110_공유기설치.py
@@ -8,22 +8,40 @@ for _ in range(n) :
     o = int(sys.stdin.readline())
     lst.append(o)
 
+#여기까쥘 입력
+lst.sort()
 
 start = min(lst)
 end = max(lst)-min(lst)
+#왼쪽과 오른쪽을 나눠 start , end로 변수 지정.
+#특이점은 end에서 집이 0에 없고 1이나 2부터 있을 수 있으니 -min(lst)를 해줌.
+
+result = []
+#결과 담을 배열
 
 while start <= end :
     mid =(start + end) // 2
-    p=0
-    cnt=0
-    for i in lst :
-        pre = lst[p]
-        if mid + pre <= i :
-            p+=1
-            cnt+=1
-    if c <= cnt :
-        end = mid - 1
-    else :
-        start = mid + 1
+    #mid 값을 구함. 이 값이 곧 공유기의 설치 거리가 될거임
+    cnt=1
+    pre = lst[0]
 
-print(end)
+    for i in lst :
+        if mid + pre <= i :
+            pre=i
+            cnt+=1
+
+    if c > cnt :
+            end = mid - 1
+            
+    else :
+            start = mid + 1
+            result.append(mid) 
+
+print(max(result))
+
+
+#c >= cnt 공유기가 부족한 경우 혹은 갯수가 딱 맞는 순간
+#이 경우에 mid 값을 감소 시키면서 공유기를 세워둠
+#mid는 공유기 간의 거리임.
+
+#c < cnt 공유기가 과다한 상황
