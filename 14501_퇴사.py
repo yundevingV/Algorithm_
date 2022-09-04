35a0856b0a5b8b89911502dea42dd72c94bae079
ys0222 <ggdbstjd0222@gmail.com>
2022-09-04 20:41:24 +0900

    0904

--- 14501_퇴사.py
+++ 14501_퇴사.py
@@ -2,3 +2,16 @@ import sys
 
 n = int(sys.stdin.readline())
 
+d=[0] * (n+1)
+
+#t는 시간 p는 보상
+lstt = [0]
+lstp = [0]
+for _ in range(n) :
+    t,p = map(int, sys.stdin.readline().split())
+    lstt.append(t)
+    lstp.append(p)
+
+
+#d[n] = max(d[n+1] , lstp[n]+d[n+lstt[n]])
+
