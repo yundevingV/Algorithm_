2a93526fb5e31492cab10adee9821f965e3157ae
ys0222 <ggdbstjd0222@gmail.com>
2022-09-07 12:47:13 +0900

    0907

--- 18353_병사배치하기.py
+++ 18353_병사배치하기.py
@@ -6,11 +6,10 @@ d = [1] * (n+1)
 
 lst = list(map(int, sys.stdin.readline().split()))
 
-for i in range(n-1) :
-    if lst[i] > lst[i+1] : 
-        d[i] = max(d[i], d[i+1] + 1 + d[i])
-    else :
-        d[i] = max(d[i], d[i-1])
+for i in range(1,n) :
+    for j in range(0,i) :
+        if lst[i] < lst[j] : 
+            d[i] = max(d[i] , d[j] + 1 )
 
-print(d)
+
 print(len(lst) - max(d))
