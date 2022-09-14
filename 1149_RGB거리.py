9b4eebf5dc7d05628fa5c0af278b47e95d954277
ys0222 <ggdbstjd0222@gmail.com>
2022-09-14 18:50:26 +0900

    0914

--- 1149_RGB거리.py
+++ 1149_RGB거리.py
@@ -2,13 +2,19 @@ import sys
 
 n = int(sys.stdin.readline())
 
-d = [[0] * (n) for _ in range(n)]
-
 g = []
 
 for i in range(n) :
     s = list(map(int, sys.stdin.readline().split()))
     g.append(s)
 
+#빨강 초록 파랑으로 다 칠해줘야함
+
+for i in range(1,n) :
+    g[i][0] = min(g[i-1][1] , g[i-1][2]) + g[i][0]
+    g[i][1] = min(g[i-1][0] , g[i-1][2]) + g[i][1]
+    g[i][2] = min(g[i-1][1] , g[i-1][0]) + g[i][2]
 
 print(g)
+print(min(g[n-1]))
+    
