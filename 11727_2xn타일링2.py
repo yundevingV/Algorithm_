f8443f77ab09dee667fd5aad6a4e97220bf9bfe9
ys0222 <ggdbstjd0222@gmail.com>
2022-09-20 14:35:45 +0900

    0920

---
+++ 11727_2xn타일링2.py
@@ -0,0 +1,16 @@
+import sys
+
+x = int(sys.stdin.readline())
+
+d = [0] * (x+1)
+
+for i in range(1,x+1) :
+    if i == 1 :
+        d[1] = 1 
+    elif i == 2 :
+        d[2] = 3
+    else : 
+        d[i] = (d[i-1] + (d[i-2]*2)) % 10007
+
+
+print(d[x])
---
+++ 2156_포도주시식.py
@@ -0,0 +1,21 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+d = [0] * (n+1)
+
+lst = []
+
+for _ in range(n) :
+    x = int(sys.stdin.readline())
+    lst.append(x)
+
+
+
+for i in range(3,n) :
+    d[0] = lst[0]
+    d[1] = lst[1]
+    d[2] = lst[2]
+    d[i] = max(d[i-3] + d[i-2] , d[i-2] + lst[i] , d[i-3]) 
+    
+print(max(d)) 
