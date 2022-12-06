646ba3eeb5ad40419ed13ab667adb28497ea2ac2
ys0222 <ggdbstjd0222@gmail.com>
2022-12-06 17:54:14 +0900

    1206

--- notepad.py
+++ notepad.py
@@ -1,9 +1,10 @@
-from os import truncate
-
-a='12'
-
-a = int(a)
-
-b= a*10
-
-print(b)
+import sys
+n = int(sys.stdin.readline())
+a = [0]*10000
+for _ in range(n):
+    num = int(sys.stdin.readline())
+    a[num]+= 1  
+for i in range(10000):
+  if a[i] != 0:
+    for j in range(a[i]):
+      print(i+1)
