fca31deceecd379f7662b0ea9635b9bfee915ec7
yundeving <91381230+yundevingV@users.noreply.github.com>
2022-12-24 19:15:42 +0900

    [Silver III] Title: N과 M (2), Time: 172 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -0,0 +1,44 @@
+let fs = require('fs');
+const filePath = process.platform === "linux" ? `/dev/stdin` : `예제.txt`; 
+let input = fs.readFileSync(filePath).toString().trim().split(' ');
+const c = console.log
+
+let lst = []
+
+for (let i=0 ; i < input.length ; i++){
+    lst.push(Number(input[i]))
+}
+
+n = lst[0]
+m = lst[1]
+
+let visited = []
+
+for(let i = 0 ; i< n+1 ; i++){
+    visited[i] = false
+}
+
+let output = []
+let result = ''
+
+dfs = (count) => {
+    if (count === m+1) {
+        result += `${output.join(' ')}\n`
+    }
+    
+    for (let i = 1; i < n+1 ; i++) {
+
+
+        if (visited[i] === true) continue
+        else if ((i > output[output.length-1]) || (output.length === 0)) {
+            visited[i] = true
+            output.push(i)
+            dfs(count+1)
+            output.pop()
+            visited[i] = false
+        }
+        }
+    }
+
+dfs(1)
+c(result.trim())
