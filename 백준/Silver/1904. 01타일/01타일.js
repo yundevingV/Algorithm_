ebd9f3e04aad54bbf29d5b38b42e7b1f46186516
yundeving <91381230+yundevingV@users.noreply.github.com>
2022-12-28 17:33:36 +0900

    [Silver III] Title: 01타일, Time: 288 ms, Memory: 42068 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.js
@@ -0,0 +1,21 @@
+let fs = require('fs');
+const filePath = process.platform === "linux" ? `/dev/stdin` : `예제.txt`; 
+let input = fs.readFileSync(filePath).toString().trim().split(' ');
+const c = console.log
+
+n = Number(input[0])
+
+let dp = []
+
+dp[1] = 1 
+dp[2] = 2
+dp[3] = 3
+
+for(let i = 4 ; i < 1000001 ; i++){
+    dp[i] = (dp[i-2] ) + (dp[i-1] )
+    if (dp[i] >= 15476) {
+        dp[i] = Number(dp[i]%15746)
+    }
+}
+
+c(dp[n])
