160e07dd8ed1383bdd1740382f93803e6c97deb7
yundeving <91381230+yundevingV@users.noreply.github.com>
2024-05-15 23:53:36 +0900

    [level 2] Title: 짝지어 제거하기, Time: 86.42 ms, Memory: 14.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,12 @@
+def solution(s):
+    stack = [];
+    for i in s :
+        if(stack and stack[-1] == i) :
+            stack.pop();
+        else :
+            stack.append(i); 
+        
+    if stack :
+        return 0;
+    else :
+        return 1;
