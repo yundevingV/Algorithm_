4014fd641098004dd8ddf906cefa57c602713abc
yundeving <91381230+yundevingV@users.noreply.github.com>
2024-05-15 17:23:02 +0900

    [level 2] Title: 피보나치 수, Time: 20.24 ms, Memory: 13.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = 0
+    dp = [0]*100001;
+    dp[1] = 1;
+    
+    for i in range(2,n+1) :
+        dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+    answer = dp[n];
+    return answer
