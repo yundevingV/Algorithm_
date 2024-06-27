c89dec3d3821cfb3c9a6ceedf43990e341fb8162
yundeving <91381230+yundevingV@users.noreply.github.com>
2024-06-28 01:43:55 +0900

    [level 2] Title: 타겟 넘버, Time: 2.67 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,18 +1,18 @@
-answer = 0;
-def DFS(numbers, current_sum, index, target):
-    global answer;
-    if index == len(numbers):
-        if current_sum == target : 
-            answer +=1;
-        else :
-            return 0;
-    
-    else:
-        DFS(numbers,current_sum + numbers[index], index + 1, target);
-        DFS(numbers,current_sum - numbers[index], index + 1, target);
-        
+answer = 0
 
+def dfs (numbers,num,index,target) :
+    global answer
+    if index == len(numbers) :
+        if num == target :
+            answer += 1
+        else :
+            return 0 
+    else : 
+        dfs(numbers, num + numbers[index], index+1,target)
+        dfs(numbers, num - numbers[index], index+1,target)
+    
 def solution(numbers, target):
-    global answer;
-    DFS(numbers, 0, 0, target);
-    return answer;
+    global answer
+    dfs(numbers,0,0,target)
+    
+    return answer
