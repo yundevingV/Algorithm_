d2775558f5dcc64f220ca6af8bef6eb928c34dec
yundeving <91381230+yundevingV@users.noreply.github.com>
2024-06-29 07:35:06 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 19.45 ms, Memory: 46.9 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,30 +1,26 @@
-
-
 function solution(q1, q2) {
-    var answer = 0;
-
-    let q = [...q1,...q2];
-    let sum1 = q1.reduce((prev, cur) => prev + cur, 0);
-    let sum2 = q2.reduce((prev, cur) => prev + cur, 0);
-    let middle = (sum1 + sum2) / 2;
-
     
-    let p1 = 0;
-    let p2 = q1.length;
+    let q = [...q1,...q2]
     
-    for(let i = 0; i < q.length * 2 ; i++){
-
-        if(sum1 === middle){return i;}
-        
-        if(sum1 > middle) {
+    let sum1 = q1.reduce((p,c) => p+c,0)
+    let sum2 = q2.reduce((p,c) => p+c,0)
+    console.log(sum1,sum2)
+    let middle = (sum1 + sum2) / 2
+    // 투 포인터
+    let p1 = 0
+    let p2 = q1.length
+    
+    for(let i =0 ; i < q.length * 2 ; i++){
+        if (sum1 == middle) { return i}
+        else if (sum1 > middle) {
             sum1 -= q[p1 % q.length]
-            p1 +=1;
+            p1 += 1
         }
-        else {
+        else if (sum1 < middle) {
             sum1 += q[p2 % q.length]
-            p2+=1;
+            p2 += 1
         }
     }
+    
     return -1;
-
 }
