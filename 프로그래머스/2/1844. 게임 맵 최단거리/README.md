7d79df81102c082963db597cd5536ab3c5e88a09
yundeving <91381230+yundevingV@users.noreply.github.com>
2024-07-10 16:09:18 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.25 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=javascript) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 6.00 ms
+메모리: 37.8 MB, 시간: 7.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 28일 01:32:23
+2024년 07월 10일 16:09:16
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,32 +1,32 @@
 function solution(maps) {
-
-    let targetX = maps.length-1;
-    let targetY = maps[0].length-1;
+    var answer = 0;
     
-    let dx = [1,-1,0,0];
-    let dy = [0,0,1,-1];
+    let dx = [1, -1, 0, 0];
+    let dy = [0, 0, 1, -1];
+    let lenX = maps.length
+    let lenY = maps[0].length
+    let q = []
+    q.push([0,0])
     
-    let queue = [];
+    let visited = Array.from({ length: maps.length }, 
+            () => Array(maps[0].length).fill(false));
 
-    queue.push([0,0,1]);
-    
-    while(queue.length > 0){
-
-        let [x,y,count] = queue.shift();
-
-        if(x === targetX && y === targetY){
-            return count;
-        }
-
-        for(let i=0;i<4;i++){
-            let nx = x + dx[i];
-            let ny = y + dy[i];
-            if(nx >= 0 && ny >= 0 && nx <= targetX && ny <= targetY && maps[nx][ny] === 1){
-                maps[nx][ny] = 0;
-                queue.push([nx,ny,count+1]);
+    while (q.length){
+        let [x,y] = q.shift()
+        visited[x][y] = true
+        for (let i =0;i<4;i++){
+            let nx = x + dx[i]
+            let ny = y + dy[i]
+            if((0<=nx) && (nx<lenX) && (0<=ny) && (ny < lenY)){
+                if(maps[nx][ny] == 1){
+                    if(!visited[nx][ny]){
+                        q.push([nx,ny])
+                        maps[nx][ny] = maps[x][y] + 1 
+                    }
+                }
             }
         }
     }
-    return -1;
-
+    
+    return maps[lenX-1][lenY-1] === 1 ? -1 : maps[lenX-1][lenY-1];
 }
