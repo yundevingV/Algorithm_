692de00fe576a2000c18ed6b20dfa3e753fd7fde
yundeving <91381230+yundevingV@users.noreply.github.com>
2024-07-10 12:07:58 +0900

    [level 2] Title: H-Index, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -0,0 +1,10 @@
+function solution(citations) {
+    var answer = 0;
+    citations.sort((a,b) => b-a)
+    for(let i =0;i<citations.length;i++){
+        if(i+1 <= citations[i]){
+            answer++
+        }
+    }
+    return answer;
+}
