e6015f9d590bac09a70791da63cfeb28be1dd343
yundeving <91381230+yundevingV@users.noreply.github.com>
2024-07-20 13:38:16 +0900

    [level 2] Title: 타겟 넘버, Time: 14.91 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,20 +1,20 @@
 function solution(numbers, target) {
     var answer = 0;
-
-    const dfs = (current_sum, idx) => {
-        if (idx === numbers.length) {
-            if (current_sum === target) {
-                answer += 1;
+    
+    const dfs = (numbers,index,sum) =>{
+        if(index === numbers.length){
+            if(sum === target){
+                answer +=1
+                return
             }
-            return;
+            else {
+                return
+            }
         }
-
-        // 현재 요소를 더하는 경우
-        dfs(current_sum + numbers[idx], idx + 1);
-        // 현재 요소를 빼는 경우
-        dfs(current_sum - numbers[idx], idx + 1);
+        dfs(numbers,index+1,sum+numbers[index])
+        dfs(numbers,index+1,sum-numbers[index])
+        
     }
-
-    dfs(0, 0);
+    dfs(numbers,0,0)
     return answer;
 }
