076085c2b10e31e45d94a152e487938a599b6310
yundeving <91381230+yundevingV@users.noreply.github.com>
2024-07-20 14:00:25 +0900

    [level 3] Title: 네트워크, Time: 0.47 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,21 +1,22 @@
 function solution(n, computers) {
     var answer = 0;
-    let v = Array(n+1).fill(false)    
-    const dfs = (idx,v) => {
-        v[idx] = true
-        for(let i = 0;i<computers[idx].length;i++){
-            if((i !== idx) && (computers[idx][i] === 1) &&(!v[i]) ){
-                dfs(i,v)
+    let visited = new Array(n).fill(false);
+
+    const dfs = (index) => {
+        visited[index] = true;
+        for (let i = 0; i < n; i++) {
+            if (computers[index][i] === 1 && !visited[i]) {
+                dfs(i);
             }
         }
+    };
+
+    for (let i = 0; i < n; i++) {
+        if (!visited[i]) {
+            dfs(i);
+            answer += 1;
+        }
     }
-   for(let i =0;i<computers.length;i++){
-       if(!v[i]){
-            dfs(i,v)    
-            answer+=1
-       }
+
+    return answer;
 }
-    
-    return answer; 
-    }
-    
