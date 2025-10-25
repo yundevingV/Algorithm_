eda5347d54dcf2a18025bda6e179bd83d10b0ec7
yundevingV <91381230+yundevingV@users.noreply.github.com>
2025-10-26 00:03:53 +0900

    [level 2] Title: 주식가격, Time: 30.43 ms, Memory: 44.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=javascript) 
 
 ### 성능 요약
 
-메모리: 48 MB, 시간: 38.41 ms
+메모리: 44.6 MB, 시간: 30.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 10일 21:55:25
+2025년 10월 26일 00:03:53
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,26 +1,16 @@
 function solution(prices) {
     var answer = [];
-    let stack = [];
     
-    const len = prices.length;
-    
-    for(let i=0; i<len - 1; i++){
-        
+    for(let i=0;i<prices.length;i++) {
         let count = 0;
-        stack.push(prices[i]);
-        
-        for(let j=i+1; j<len; j++){
-            if(stack[stack.length - 1] > prices[j]){
+        for(let j=i+1;j<prices.length;j++) {
+            if(prices[i] > prices[j]) {
                 count += 1;
                 break;
-            }   
-            else {
-                count += 1;
             }
+            else count += 1;
         }
         answer.push(count);
     }
-    answer.push(0);
-
     return answer;
 }
